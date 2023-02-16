a50a59b1edf14df1f92875c33ac60b6d6630d895
Nathan Walston <walston@users.noreply.github.com>
2023-02-15 20:21:19 -0800

    basics of layout

--- src/caddie.tsx
+++ src/caddie.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import "./style.css";
-
 function CaddieApp() {
-  return <div>Hello</div>;
+  return (
+    <aside>
+      <h1>Onthrop</h1>
+      <div onClick={() => console.log("rolling")} style={{ cursor: "pointer" }}>
+        <h2>Longsword (one-handed)</h2>
+        <pre contentEditable>
+          (1d20;n=20:CRIT,n=1:FAIL)|(slashing,1d8)(CRIT?slashing,1d8)
+        </pre>
+      </div>
+      <div onClick={() => console.log("rolling")} style={{ cursor: "pointer" }}>
+        <h2>Longsword (two-handed)</h2>
+        <pre contentEditable>
+          (1d20;n=20:CRIT,n=1:FAIL)|(slashing,1d10)(CRIT?slashing,1d10)
+        </pre>
+      </div>
+    </aside>
+  );
 }
 
 const mountpoint = document.getElementById("caddie") as HTMLDivElement;
