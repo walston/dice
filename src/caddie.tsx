1aefa3fbd09ff67b28eb07db1096103d30373689
Nathan Walston <walston@users.noreply.github.com>
2023-02-15 20:33:22 -0800

    make a new type for Rolls

--- src/caddie.tsx
+++ src/caddie.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+type RollString = string;
+type Roll = { name: string; roll: RollString };
+
+const ROLLS: Roll[] = [
+  {
+    name: "Longsword (one-handed)",
+    roll: "(1d20;n=20:CRIT,n=1:FAIL)|(slashing,1d10)(CRIT?slashing,1d10)",
+  },
+  {
+    name: "Longsword (two-handed)",
+    roll: "(1d20;n=20:CRIT,n=1:FAIL)|(slashing,1d8)(CRIT?slashing,1d8)",
+  },
+];
+
 function CaddieApp() {
   return (
     <aside>
       <h1>Onthrop</h1>
-      <div onClick={() => console.log("rolling")} style={{ cursor: "pointer" }}>
-        <h2>Longsword (one-handed)</h2>
-        <pre contentEditable>
-          (1d20;n=20:CRIT,n=1:FAIL)|(slashing,1d8)(CRIT?slashing,1d8)
-        </pre>
-      </div>
-      <div onClick={() => console.log("rolling")} style={{ cursor: "pointer" }}>
-        <h2>Longsword (two-handed)</h2>
-        <pre contentEditable>
-          (1d20;n=20:CRIT,n=1:FAIL)|(slashing,1d10)(CRIT?slashing,1d10)
-        </pre>
-      </div>
+      {ROLLS.map(({ name, roll }) => (
+        <div
+          onClick={() => console.log("rolling", name)}
+          style={{ cursor: "pointer" }}
+        >
+          <h2>{name}</h2>
+          <pre contentEditable>{roll}</pre>
+        </div>
+      ))}
     </aside>
   );
 }
