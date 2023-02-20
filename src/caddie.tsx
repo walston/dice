90e6ba5b291ec659d4a0fd26e491bb613dfbdc4d
Nathan Walston <walston@users.noreply.github.com>
2023-02-19 17:14:01 -0800

    creates a roll service

--- src/caddie.tsx
+++ src/caddie.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { loadCharacter } from "./characterService";
+import { roll } from "./rollService";
 
 function CaddieApp() {
   const { name, rolls } = loadCharacter();
@@ -8,10 +9,7 @@ function CaddieApp() {
     <aside>
       <h1>{name}</h1>
       {rolls.map(({ title, script }) => (
-        <div
-          onClick={() => console.log("rolling", title)}
-          style={{ cursor: "pointer" }}
-        >
+        <div onClick={() => roll(script)} style={{ cursor: "pointer" }}>
           <h2>{title}</h2>
           <pre contentEditable>{script}</pre>
         </div>
---
+++ src/rollService.ts
@@ -0,0 +1,3 @@
+export function roll(script: string) {
+  console.log(script)
+}
