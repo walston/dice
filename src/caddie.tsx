6a34693a80b18e218534a76bfb32473f8064b0ec
Nathan Walston <walston@users.noreply.github.com>
2023-02-20 13:40:52 -0800

    Deep overhaul of character service

--- src/caddie.tsx
+++ src/caddie.tsx
@@ -1,19 +1,50 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { loadCharacter } from "./characterService";
+import { useCharacter } from "./characterService";
 import { roll } from "./rollService";
+import "./caddie.css";
 
 function CaddieApp() {
-  const { name, rolls } = loadCharacter();
+  const [{ name, rolls }, dispatchers] = useCharacter();
+
   return (
     <aside>
-      <h1>{name}</h1>
-      {rolls.map(({ title, script }) => (
-        <div onClick={() => roll(script)} style={{ cursor: "pointer" }}>
-          <h2>{title}</h2>
-          <pre contentEditable>{script}</pre>
+      <h1>
+        {" "}
+        <input
+          className="inherit"
+          value={name}
+          onChange={(event) => dispatchers.renameCharacter(event.target.value)}
+        />
+      </h1>
+      {rolls.map(({ guid, title, script }) => (
+        <div
+          key={guid}
+          onClick={() => roll(script)}
+          style={{ cursor: "pointer" }}
+        >
+          <h2>
+            <input
+              className="inherit"
+              value={title}
+              onChange={(event) =>
+                dispatchers.renameRoll(guid, event.target.value)
+              }
+            />
+          </h2>
+          <pre>
+            <textarea
+              className="inherit"
+              rows={1}
+              value={script}
+              onChange={(event) =>
+                dispatchers.changeRoll(guid, event.target.value)
+              }
+            />
+          </pre>
         </div>
       ))}
+      <button onClick={() => dispatchers.newRoll("", "")}>+</button>
     </aside>
   );
 }
--- src/characterService.ts
+++ src/characterService.ts
@@ -1,22 +1,106 @@
+import { useReducer } from 'react';
+import { v4 } from 'uuid';
+
 export interface Roll {
+  guid: string;
   title: string;
   /** script should eventually be a TROLL script */
   script: string;
 }
 
 export interface Character {
+  guid: string;
   name: string;
   rolls: Roll[]
 }
 
-export function loadCharacter(): Character {
-  /** @todo save these in `window.localstorage` for the time being */
-  return {
-    name: 'Onthrop',
-    rolls: [
-      { title: 'Longsword', script: '1d8+4' },
-      { title: 'Longsword (2-handed)', script: '2d8+4' }
-    ]
+type Action<Name, Payload> = { type: Name, payload: Payload };
+type CharacterActions = |
+  Action<'new character', { name: string, }> |
+  Action<'rename character', { name: string }> |
+  // Action<'delete character', never> |
+  Action<'new roll', Partial<{ title: string, script: string }>> |
+  Action<'rename roll', { guid: string, title: string }> |
+  Action<'change roll', { guid: string, script: string }> |
+  Action<'delete roll', { guid: string }>;
+type CharacterReducer = React.Reducer<Character, CharacterActions>;
+
+const reducer: CharacterReducer = (state, { type, payload }) => {
+  let character: Character;
+  switch (type) {
+    case 'new character':
+      return characterFactory(payload.name);
+    case 'rename character':
+      character = { ...state };
+      character.name = payload.name;
+      return character;
+    // case 'delete character':
+    //   return characterFactory();
+    case 'new roll':
+      character = { ...state };
+      character.rolls.push(rollFactory(payload));
+      return character;
+    case 'rename roll':
+    case 'change roll':
+      character = { ...state };
+      const roll = character.rolls.find(({ guid }) => guid === payload.guid);
+      if (!roll) return state;
+      Object.assign(roll, payload)
+      return character;
+    case 'delete roll':
+      const rolls = state.rolls.filter(({ guid }) => guid === payload.guid);
+      if (rolls.length === state.rolls.length) return state;
+      character = { ...state, rolls };
+      return character;
+    default:
+      const never: never = payload
+      return state;
   }
 }
 
+const defaultCharacter = characterFactory('Onthrop');
+defaultCharacter.rolls.push(rollFactory({ title: 'Longsword', script: 'd8' }))
+defaultCharacter.rolls.push(rollFactory({ title: 'Longsword (two-handed)', script: 'd10' }))
+
+export function useCharacter() {
+  const [character, dispatch] = useReducer(reducer, defaultCharacter);
+  return [
+    character,
+    {
+      newCharacter(name: string) {
+        dispatch({ type: 'new character', payload: { name } });
+      },
+      renameCharacter(name: string) {
+        dispatch({ type: 'rename character', payload: { name } })
+      },
+      newRoll(title: string, script: string) {
+        dispatch({ type: 'new roll', payload: { title, script } });
+      },
+      renameRoll(guid: string, title: string) {
+        dispatch({ type: 'rename roll', payload: { guid, title } });
+      },
+      changeRoll(guid: string, script: string) {
+        dispatch({ type: 'change roll', payload: { guid, script } });
+      },
+      deleteRoll(guid: string) {
+        dispatch({ type: 'delete roll', payload: { guid } });
+      }
+    }
+  ] as const
+}
+
+function characterFactory(name?: string): Character {
+  const guid = v4();
+  return {
+    guid,
+    name: name || '',
+    rolls: []
+  }
+}
+
+function rollFactory(roll: Partial<Omit<Roll, 'guid'>>): Roll {
+  const guid = v4();
+  const title = roll.title || '';
+  const script = roll.script || '';
+  return { guid, title, script };
+}
