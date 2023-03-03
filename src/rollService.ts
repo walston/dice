f4020e2ea64e0558ae9bef863314fb7b5a384236
Nathan Walston <walston@users.noreply.github.com>
2023-03-03 13:51:43 -0800

    Build in rudimentary TROLL syntax support

--- src/rollService.ts
+++ src/rollService.ts
@@ -1,3 +1,19 @@
+import peg from 'peggy'
+import TROLL_GRAMMAR from './troll.peggy?raw'
+
+let languageParser: peg.Parser;
+
+try {
+  languageParser = peg.generate(TROLL_GRAMMAR);
+} catch (e) {
+  console.error(e);
+}
+
 export function roll(script: string) {
-  console.log(script)
+  try {
+    const ast = languageParser.parse(script);
+    console.log(ast)
+  } catch (e) {
+    console.log(e)
+  }
 }
